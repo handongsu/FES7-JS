fc1fcca4dedbfd7a2903fefad940e6b5f836ecdc
dongsu.han <kku304@naver.com>
2023-08-17 20:06:12 +0900

    20230817 수업

--- 수업/20230816/002.js
+++ 수업/20230816/002.js
@@ -9,21 +9,34 @@ man.sayHi();    //안녕하세요 저는 한동수
 
 //문제2
 var 자료 = {
-    data : [1,2,3,4,5]
+    data : [1,2,3,4,5],
 }
-
+console.log(자료)
 자료.전부더하기 = function(){
-    let sum = 0;
-    this.data.forEach(function(a){
-        sum = sum + a;
-    });
-    console.log(sum)
+    console.log(this)
+    console.log(this.data.reduce((a,c) => a+c, 0))
 }
-
+console.log(자료)
 자료.전부더하기();
+
+// 자료.전부더하기 = function(){
+//     let sum = 0;
+//     console.log(this)
+//     this.data.forEach(function(a){
+//         sum = sum + a;
+//     });
+//     console.log(sum)
+// }
 
 
 // function 기계(){
 //   this.이름 = 'Kim'
 // }
 // var 오브젝트 = new 기계();
+
+const num = 12345;
+
+function answer(num){
+    return num.toString().split('').reverse().map(c => c * 2);
+}
+answer();
---
+++ 수업/20230817/001.js
@@ -0,0 +1,11 @@
+function addElements(arr){
+    if(arr.length === 0){
+        return 0
+    }
+    arr[0] + addElements(arr.slice(1));
+}
+addElements([1,2,3,4]);
+
+let obj = {one: 1, two : 2};
+let {one: myNum, two: myNum2} = {one: 1, two : 2};
+console.log(myNum, myNum2);
